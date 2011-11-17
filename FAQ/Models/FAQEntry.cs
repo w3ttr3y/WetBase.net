04e037b6ccaa1ee503b2e029c011b9c200c688d3
William E. Triest III <w3ttr3y@w3ttr3y.com>
2011-11-16 23:01:19 -0500

    Updated FAQEntry so answer has a MultiLineText ui hint; This should cause EditorFor to use <textarea> instead of <input type="text">

--- FAQ/Models/FAQEntry.cs
+++ FAQ/Models/FAQEntry.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -9,6 +10,8 @@ namespace FAQ.Models
     {
         public int FAQEntryId { get; set; }
         public string Question { get; set; }
+
+        [UIHint("MultiLineText")]
         public string Answer { get; set; }
     }
 }
